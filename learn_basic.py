8d6982cb9e6d83dad97be36609c942cdfe7e1126
santis_yang <bingjilin@me.com>
2017-05-21 23:06:26 -0400

    add cuda support

--- learn_basic.py
+++ learn_basic.py
@@ -5,8 +5,14 @@ from torch.autograd import Function
 
 
 """
-TODO: 1. For now, not Support Batch
-TODO: 2, the leverage of numpy shared memory here will not work in cuda tensor
+TODO: 1. For now, not Support Batch: to resolve this problem, we should wrap a loop outside this function
+TODO: 2. the leverage of numpy shared memory here will not work in cuda tensor: not so nice solution is from the 
+        beginning of entrance of the function, check if they are in GPU; transfer them to CPU without exception and 
+        to see if we need to transfer them back to GPU according to situation
+TODO: 3. convert input to double
+
+Note:
+      1. Here is calculating the log distribution, not the original distribution
 """
 
 class MVNLoss(Function):
@@ -17,18 +23,30 @@ class MVNLoss(Function):
         self.dim = None
         self.input_minus_miu = None
         self.inv_sing_mat = None
+        self.CUDA_FLAG = None
 
     def forward(self, params, input):
         """
-        
-        :param ctx: 
         :param params: vector miu and unique elements of covariance matrix sigma
                        the first dim number of params is miu and then is sigma
         :param input: ground truth samples from the training set  
         :param dim: the dimension of the distribution
-        :return: 
+        :return: forward output
         """
-        self.save_for_backward(params, input)
+        # self.save_for_backward(params, input)
+
+        try:
+            if params.is_cuda and input.is_cuda:
+                CUDA_FLAG = True
+            elif not params.is_cuda and not input.is_cuda:
+                CUDA_FLAG = False
+            else:
+                raise ValueError("params and input are not in the same kind of device")
+        except ValueError as err:
+            print(err.message)
+
+        params = params.cpu().double()
+        input = input.cpu().double()
 
         dim = np.prod(input.size())
         param_dim = np.prod(params.size())
@@ -53,11 +71,15 @@ class MVNLoss(Function):
         self.dim = dim
         self.input_minus_miu = input_minus_miu
         self.inv_sing_mat = inv_sing_mat
+        self.CUDA_FLAG = CUDA_FLAG
 
         output = - dim/2.0 * np.log(2*np.pi) + (-1.0/2) * np.log(torch.prod(sing_values)) + \
                  (-1.0/2)*torch.mm(torch.mm(input_minus_miu, inv_sing_mat), input_minus_miu.t())
 
         # the output is actually the log pdf
+        if CUDA_FLAG:
+            output = output.cuda()
+
         return output
 
 
@@ -65,10 +87,11 @@ class MVNLoss(Function):
         """
         Here exist two assumptions:
         1. the covariance matrix we construct from the output of RNN is positive definite
-        2. the parameters we get from RNN through this operation is 1xdim, while the input is dimx1
+        2. the parameters we get from RNN through this operation is [1, ~], while the input is [dim, 1]
         """
         # params, input = self.saved_variables
-        grad_output = grad_output[0, 0]
+
+        grad_output = grad_output[0, 0] # extract the scalar from PyTorch tensor
 
         inv_sing_mat = self.inv_sing_mat
         input_minus_miu = self.input_minus_miu
@@ -80,7 +103,9 @@ class MVNLoss(Function):
         mid_result1 = torch.mm(input_minus_miu.t(), input_minus_miu)
 
         grad_sigma1 = -torch.mm(torch.mm(inv_sing_mat, mid_result1), inv_sing_mat)
+        # print(grad_sigma1)
         grad_sigma2 = inv_sing_mat
+        # print(grad_sigma2)
         grad_sigma = -(1.0/2)*(grad_sigma1 + grad_sigma2)*grad_output
 
         grad_sigma_np = grad_sigma.numpy()
@@ -95,5 +120,9 @@ class MVNLoss(Function):
 
         # gradients of input
         grad_input = -torch.mm(inv_sing_mat, input_minus_miu.t())*grad_output
+
+        if self.CUDA_FLAG:
+            grad_params = grad_params.cuda()
+            grad_input = grad_input.cuda()
 
         return grad_params, grad_input
--- test.py
+++ test.py
@@ -3,15 +3,34 @@ from torch.autograd import Variable
 from torch.autograd import gradcheck
 from learn_basic import MVNLoss
 
-input = (Variable(torch.Tensor([0, 0, 3, 2, 3]).double().view(1, 5), requires_grad=True),
+# Test one dimension MVN
+# input = (Variable(torch.Tensor([0, 4]).double().view(1, 2), requires_grad=True),
+#          Variable(torch.Tensor([1,]).double().view(1, 1), requires_grad=True))
+#
+# test = gradcheck(MVNLoss(), input, eps=1e-6, atol=1e-4)
+
+# print("gradient check of one-dimension test: {}".format(test))
+
+# Test two dimension MVN
+input = (Variable(torch.Tensor([0, 0, 8, 2, 8]).double().view(1, 5), requires_grad=True),
          Variable(torch.Tensor([1, 1]).double().view(2, 1), requires_grad=True))
 
 test = gradcheck(MVNLoss(), input, eps=1e-6, atol=1e-4)
 
-print(test)
+print("gradient check of two-dimension test: {}".format(test))
 
-# input = (torch.Tensor([0, 0, 3, 2, 3]).double().view(1, 5),
+# explicitly show the actual calculated forward result and backward gradient
+# input = (torch.Tensor([0, 0, 8, 2, 8]).double().view(1, 5),
 #          torch.Tensor([1, 1]).double().view(2, 1))
+#
+# mvnLoss = MVNLoss()
+#
+# print(mvnLoss.forward(input[0], input[1]))
+# print(mvnLoss.backward(torch.Tensor([[1.0,]])))
+
+
+# input = (torch.Tensor([0, 3]).double().view(1, 2),
+#          torch.Tensor([1,]).double().view(1, 1))
 #
 # mvnLoss = MVNLoss()
 #
